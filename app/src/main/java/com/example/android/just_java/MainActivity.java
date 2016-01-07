4c9db155c71d10f226ddf04146f2ee11e9cb6b38
Ajdin Imsirovic <ajdin@leftor.ba>
2016-01-07 21:05:09 +0100

    (15) Add displayPrice method to MainActivity.java (16) Here, NumberFormat is not recognized.
        Make sure Auto Import is on:
    
            For Windows, go to File > Settings > Editor > General > Auto Import
            For Mac, go to Android Studio > Preferences > Editor > General > Auto Import
            Check all of the following options:
    
            Show import popup
            Optimize imports on the fly
            Add unambiguous imports on the fly
            Insert imports on paste → All

--- app/src/main/java/com/example/android/just_java/MainActivity.java
+++ app/src/main/java/com/example/android/just_java/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 /**
  * This app displays an order form to order coffee.
  */
@@ -30,5 +32,13 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
