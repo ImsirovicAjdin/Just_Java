0438da568121a8345b4fd87d9969546b019295db
Ajdin Imsirovic <ajdin@leftor.ba>
2016-01-08 19:34:22 +0100

    (35) Changed the code to say "free" after the order button has been clicked.

--- app/src/main/java/com/example/android/just_java/MainActivity.java
+++ app/src/main/java/com/example/android/just_java/MainActivity.java
@@ -40,7 +40,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(quantity * 5);
+        String priceMessage = "Free";
+        displayMessage(priceMessage);
+        // displayPrice(quantity * 5);
     }
 
     /**
@@ -59,4 +61,13 @@ public class MainActivity extends AppCompatActivity {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+
 }
