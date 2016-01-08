cda81ff1c607d62ae9c5ce851f95e84e732559d6
Ajdin Imsirovic <ajdin@leftor.ba>
2016-01-09 00:15:30 +0100

    (46) Add another param to createWeatherMessage() method declaration - cityName. (47) Also pass this second param to the method call. (48) And also add the @param to the method declaration's comment block

--- app/src/main/java/com/example/android/just_java/MainActivity.java
+++ app/src/main/java/com/example/android/just_java/MainActivity.java
@@ -19,14 +19,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        createWeatherMessage(77);
+        createWeatherMessage(77, "San Francisco");
     }
 
     /**
      * Create new message that specifies the city name and temperature.
+     *
+     * @param temperature of the city
+     * @param cityName is the name of the city
      */
-    private String createWeatherMessage(int temperature) {
-        return "Welcome to San Francisco where the temperature is " + temperature + "degrees F";
+    private String createWeatherMessage(int temperature, String cityName) {
+        return "Welcome to " + cityName + " where the temperature is " + temperature + "degrees F";
     }
 
     /**
