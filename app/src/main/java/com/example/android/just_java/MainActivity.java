2b074beca5ed3fe083cd76427e4f35b18d1938ce
Ajdin Imsirovic <ajdin@leftor.ba>
2016-01-12 22:24:40 +0100

    (76.hrs56-64) Correct the <EditText> view, (77.hrs56-64) Correct the comments in the submitOrder() method call

--- app/src/main/java/com/example/android/just_java/MainActivity.java
+++ app/src/main/java/com/example/android/just_java/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.android.just_java;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 /**
@@ -40,8 +42,15 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        EditText nameField = (EditText) findViewById(R.id.name_field);
+        String name = nameField.getText().toString();
+        Log.v("MainActivity", "Name: " + name);
+
+        // Figure out if the user wants whipped cream topping
         CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+
+        // Figure our if the user wants chocolate topping
         CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
         boolean hasChocolate = chocolateCheckBox.isChecked();
 
